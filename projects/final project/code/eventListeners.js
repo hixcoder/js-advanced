bec55226a0376e53cc077c875155191d988ee385
hixcoder <hixcoder@gmail.com>
2023-09-20 17:47:01 +0100

    finish the homePage design

---
+++ projects/final project/code/eventListeners.js
@@ -0,0 +1,18 @@
+const drawerCheckbox = document.getElementById("drawer");
+const drawerIcon = document.getElementById("drawer-icon");
+const nav = document.getElementsByClassName("nav");
+const navlink = document.getElementsByClassName("navlink");
+
+drawerCheckbox.addEventListener("change", () => {
+  drawerIcon.style.transition = "0.5s";
+  for (let i = 0; i < navlink.length; i++) {
+    if (drawerCheckbox.checked) {
+      navlink[i].style.marginTop = "-100%";
+      drawerIcon.className = "fa-solid fa-bars";
+    } else {
+      navlink[i].style.marginTop = "0";
+      navlink[i].style.transition = "0.5s";
+      drawerIcon.className = "fa-solid fa-x";
+    }
+  }
+});
